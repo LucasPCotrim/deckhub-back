692b138cf0adae808a74dea00ba0aef5d3140e8c
Lucas Pereira Cotrim <lucaspcotrim@gmail.com>
2023-02-02 23:26:47 -0300

    feat: Added joi validation to route GET /decks/:id

--- src/routers/deckRouter.ts
+++ src/routers/deckRouter.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
+import { validateParams } from '@/middlewares';
+import { deckIdSchema } from '@/schemas/';
 import { getDecks, getDeckInfo } from '@/controllers/deckController';
 
 const deckRouter = Router();
 
 deckRouter.get('/', getDecks);
-deckRouter.get('/:id', getDeckInfo);
+deckRouter.get('/:id', validateParams(deckIdSchema), getDeckInfo);
 
 export { deckRouter };
